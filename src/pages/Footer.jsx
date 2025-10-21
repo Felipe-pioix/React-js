1c03d5e275eafdb41e986cf6ea889d0e12f8f09f
Felipe Vazquez <Fegerva2@gmail.com>
2025-10-21 13:53:27 -0300

    first commit

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <hr />
+        <p>Máquinas y accesorios para la industria del plástico</p>
+    </footer>
+  )
+}
+
+export default Footer
